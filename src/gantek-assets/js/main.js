8ad52cd37f71c45a13ca83986b567cee9eef3895
koda <erickmodestus@gmail.com>
2025-01-29 13:11:23 +0300

    Add files via upload

---
+++ src/gantek-assets/js/main.js
@@ -0,0 +1,14 @@
+// Import AOS CSS
+import '../../../node_modules/aos/dist/aos.css';
+
+// Import purecounter
+import purecounter from ''
+
+// Import AOS JS
+import AOS from '../../../node_modules/aos';
+
+// Initialize AOS
+AOS.init();
+
+// initialize purecounter
+new purecounter();
